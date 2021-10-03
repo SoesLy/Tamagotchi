17cfdeb29828e05249c43b707e565a129d63fbe7
Søs <slys87@gmail.com>
2021-10-03 18:28:22 +0200

    Attributes

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,11 @@
-public class Cat {
+public class Cat extends Tamagotchi {
+
+    public boolean kittenOrNot;
+
+
+    public Cat(String name, int age, String mood, int energy, String color, boolean kittenOrNot) {
+        super(name, age, mood, energy, color);
+        this.kittenOrNot = kittenOrNot;
+    }
 }
+
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -4,7 +4,29 @@ public class Tamagotchi {
     public int age;
     public String mood;
     public int energy;
+    public String color;
+
+    public Tamagotchi(String name, int age, String mood, int energy, String color) {
+
+        this.name = name;
+        this.age = age;
+        this.mood = mood;
+        this.color = color;
+    }
+
+    public void doFeed() {
+
+        return;
+    }
+
+    public void doPlay() {
+
+        return;
+    }
+
+    public void doSleep() {
+
+        return;
+    }
 
 }
-
-public
